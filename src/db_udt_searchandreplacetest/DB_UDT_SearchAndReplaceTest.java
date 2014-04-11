3629013890a18694c78af840a9a7f3b28b9dfaca
deadlyBuzz <alan.pm.curley@gmail.com>
2014-04-11 23:55:45 +0100

    initial Commit Work in progress building a test application for the data build routines for the S7 program. To be thread enabled and report back using progress bars.

---
+++ src/db_udt_searchandreplacetest/DB_UDT_SearchAndReplaceTest.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db_udt_searchandreplacetest;
+
+/**
+ * A test program that will ensure that the search and replace mechanism for replacing
+ * UDTs and DB data in a source code block operates correctly.
+ * @author Alan Curley
+ */
+public class DB_UDT_SearchAndReplaceTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
